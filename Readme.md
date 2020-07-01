7fc51b6e37175030915deab4b9b258937fe94e36
Vitali V <newkinos@gmail.com>
2020-07-01 21:59:43 +0300

    Правки js

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Личный проект «Техномарт»
 
 * Студент: [Виталий Вячеславович](https://up.htmlacademy.ru/htmlcss/28/user/955829).
-* Наставник: [Малик Киров](https://htmlacademy.ru/profile/id17217).
+* Наставник: [МК](https://htmlacademy.ru/profile/id17217).
 
 ---
 
--- js/script.js
+++ js/script.js
@@ -4,79 +4,84 @@ let popupWrite = document.querySelector('.write-us');
 let closeWrite = document.querySelector('.write-us .popup-close');
 let nameWrite = document.querySelector('.write-us .name');
 
-linkWrite.onclick = function (evt) {
-  evt.preventDefault();
-  popupWrite.classList.add('popup-show');
-  nameWrite.focus();
-};
+if (linkWrite) {
+  linkWrite.onclick = function (evt) {
+    evt.preventDefault();
+    popupWrite.classList.add('popup-show');
+    nameWrite.focus();
+  };
+}
 
-closeWrite.onclick = function (evt) {
-  evt.preventDefault();
-  popupWrite.classList.remove('popup-show');
-};
+if (closeWrite) {
+  closeWrite.onclick = function (evt) {
+    evt.preventDefault();
+    popupWrite.classList.remove('popup-show');
+  };
+}
 
-window.onclick = function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     if (popupWrite.classList.contains('popup-show')) {
       evt.preventDefault();
       popupWrite.classList.remove('popup-show');
     }
   }
-};
+});
 
 // Карта на Главной
 let smallMap = document.querySelector('.map-small');
 let bigMap = document.querySelector('.big-map');
 let closeMap = document.querySelector('.big-map .popup-close');
 
-smallMap.onclick = function (evt) {
-  evt.preventDefault();
-  bigMap.classList.add('popup-show');
-};
+if (smallMap) {
+  smallMap.onclick = function (evt) {
+    evt.preventDefault();
+    bigMap.classList.add('popup-show');
+  };
+}
 
-closeMap.onclick = function (evt) {
-  evt.preventDefault();
-  bigMap.classList.remove('popup-show');
-};
+if (closeMap) {
+  closeMap.onclick = function (evt) {
+    evt.preventDefault();
+    bigMap.classList.remove('popup-show');
+  };
+}
 
-window.onclick = function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     if (bigMap.classList.contains('popup-show')) {
       evt.preventDefault();
       bigMap.classList.remove('popup-show');
     }
   }
-};
+});
 
 // Форма заказа в Каталоге
-let linkBuy = document.querySelector('.buy-button');
+let linkBuy = document.querySelectorAll('.buy-button');
 let windowBuy = document.querySelector('.cart-add');
 let closeBuy = document.querySelector('.cart-add .popup-close');
 
-linkBuy.onclick = function (evt) {
-  evt.preventDefault();
-  windowBuy.classList.add('popup-show');
-};
+if (linkBuy) {
+ for (let i = 0; i< linkBuy.length; i++) {
+  linkBuy[i].onclick = function (evt) {
+    evt.preventDefault();
+    windowBuy.classList.add('popup-show');
+  };
+ }
+}
 
-closeBuy.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  windowBuy.classList.remove('popup-show');
-});
+if (closeBuy) {
+  closeBuy.onclick = function (evt) {
+    evt.preventDefault();
+    windowBuy.classList.remove('popup-show');
+  };
+}
 
-window.onclick = function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     if (windowBuy.classList.contains('popup-show')) {
       evt.preventDefault();
       windowBuy.classList.remove('popup-show');
     }
   }
-};
-
-// Слайдер на Главной
-let sliderArrow = document.querySelectorAll('.arrow-button');
-let slide = document.querySelector('.slide');
-
-sliderArrow.onclick = function () {
-  slide.classList.toggle('slide-current');
-  slide.classList.toggle('');
-};
+});
