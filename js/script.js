2f010b6792fbe8e832dbb2ae0079098ad37d21da
Vitali V <newkinos@gmail.com>
2020-07-01 19:59:48 +0300

    Вторая защита
    Внес правки после проверки.
    По JS совсем плохо.
    Выскакиевает ошибка если элемента нет на странице и дальше не работает код.
    Попап Каталоге не понимаю как сделать, чтобы на каждую кнопку "Купить" работало. В материалах Академии как раз пропущено это в главе про qyerySelectionAll. Я им написал чтобы дополнили теорию. У них в целом по JS jxym много отсутствует для новичков и сразу задания сделай то, а в теории вообще не рассказывают что это и как использовать.

--- js/script.js
+++ js/script.js
@@ -1,47 +1,82 @@
-var linkWrite = document.querySelector(".write");
-var popupWrite = document.querySelector(".write-us");
-var closeWrite = popupWrite.querySelector(".popup-close");
-var nameWrite = popupWrite.querySelector(".name");
+// Форма обратной связи на Главной
+let linkWrite = document.querySelector('.write');
+let popupWrite = document.querySelector('.write-us');
+let closeWrite = document.querySelector('.write-us .popup-close');
+let nameWrite = document.querySelector('.write-us .name');
 
-linkWrite.addEventListener("click", function (evt) {
+linkWrite.onclick = function (evt) {
   evt.preventDefault();
-  popupWrite.classList.add("popup-show");
+  popupWrite.classList.add('popup-show');
   nameWrite.focus();
-});
+};
 
-closeWrite.addEventListener("click", function (evt) {
+closeWrite.onclick = function (evt) {
   evt.preventDefault();
-  popupWrite.classList.remove("popup-show");
-});
+  popupWrite.classList.remove('popup-show');
+};
 
-window.addEventListener("keydown", function (evt) {
+window.onclick = function (evt) {
   if (evt.keyCode === 27) {
-    if (popupWrite.classList.contains("popup-show")) {
+    if (popupWrite.classList.contains('popup-show')) {
       evt.preventDefault();
-      popupWrite.classList.remove("popup-show");
+      popupWrite.classList.remove('popup-show');
     }
   }
-});
+};
 
-var smallMap = document.querySelector(".map-small");
-var bigMap = document.querySelector(".big-map");
-var closeMap = bigMap.querySelector(".popup-close");
+// Карта на Главной
+let smallMap = document.querySelector('.map-small');
+let bigMap = document.querySelector('.big-map');
+let closeMap = document.querySelector('.big-map .popup-close');
 
-smallMap.addEventListener("click", function (evt) {
+smallMap.onclick = function (evt) {
   evt.preventDefault();
-  bigMap.classList.add("popup-show");
-});
+  bigMap.classList.add('popup-show');
+};
 
-closeMap.addEventListener("click", function (evt) {
+closeMap.onclick = function (evt) {
   evt.preventDefault();
-  bigMap.classList.remove("popup-show");
-});
+  bigMap.classList.remove('popup-show');
+};
 
-window.addEventListener("keydown", function (evt) {
+window.onclick = function (evt) {
   if (evt.keyCode === 27) {
-    if (bigMap.classList.contains("popup-show")) {
+    if (bigMap.classList.contains('popup-show')) {
       evt.preventDefault();
-      bigMap.classList.remove("popup-show");
+      bigMap.classList.remove('popup-show');
     }
   }
+};
+
+// Форма заказа в Каталоге
+let linkBuy = document.querySelector('.buy-button');
+let windowBuy = document.querySelector('.cart-add');
+let closeBuy = document.querySelector('.cart-add .popup-close');
+
+linkBuy.onclick = function (evt) {
+  evt.preventDefault();
+  windowBuy.classList.add('popup-show');
+};
+
+closeBuy.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  windowBuy.classList.remove('popup-show');
 });
+
+window.onclick = function (evt) {
+  if (evt.keyCode === 27) {
+    if (windowBuy.classList.contains('popup-show')) {
+      evt.preventDefault();
+      windowBuy.classList.remove('popup-show');
+    }
+  }
+};
+
+// Слайдер на Главной
+let sliderArrow = document.querySelectorAll('.arrow-button');
+let slide = document.querySelector('.slide');
+
+sliderArrow.onclick = function () {
+  slide.classList.toggle('slide-current');
+  slide.classList.toggle('');
+};
